383a84009dd4756ca529ccf2e66a86f650cd1c11
dheidenr <work-post@bk.ru>
2020-03-14 17:31:13 +0300

    release Line regression and kNN

--- knn.py
+++ knn.py
@@ -1,5 +1,10 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 all_data = pd.read_csv('forest_dataset.csv')
 print(all_data.head())
@@ -10,3 +15,67 @@ labels = all_data[all_data.columns[-1]].values
 
 # Столбцы'0-54' - признаковые описания
 feature_matrix = all_data[all_data.columns[:-1]].values
+
+# Разделим выборку на обучающую и тестовую
+train_feature_matrix, test_feature_matrix, train_labels, test_labels = \
+    train_test_split(feature_matrix, labels, test_size=0.2, random_state=42)
+
+
+clf = KNeighborsClassifier()
+
+clf.fit(train_feature_matrix, train_labels)
+# Ваш код здесь,
+y_pred = clf.predict(test_feature_matrix)
+print(y_pred)
+
+print(accuracy_score(test_labels, y_pred))
+# При оптимальных параметрах 0.785
+# По умолчанию 0.7365
+
+clf = KNeighborsClassifier()
+# clf.max_iter = 1000
+
+parad_grid = {
+    'n_neighbors': np.arange(1, 10),
+    'weights': ['uniform', 'distance'],
+    'metric': ['euclidean', 'manhattan'],
+}
+
+search = GridSearchCV(clf, parad_grid, cv=5, scoring='accuracy', n_jobs=-1)
+
+search.fit(feature_matrix, labels)
+
+# У меня нашло с кучей варнингов {'C': 2, 'penalty': 'l2'} ghb max_
+# Нашло на юпитере тоже в варниногом {'C': 1, 'penalty': 'l1'}
+
+# {'metric': 'manhattan', 'n_neighbors': 4, 'weights': 'distance'}
+print(search.best_params_)
+# Вычисление оптимальной точности
+clf = KNeighborsClassifier(
+    n_neighbors=4, weights='distance', metric='manhattan')
+clf.fit(train_feature_matrix, train_labels)
+y_pred = clf.predict(test_feature_matrix)
+print(y_pred)
+print(accuracy_score(test_labels, y_pred))
+
+# Вычисление прогназируемой вероятности класса
+
+optimal_clf = clf
+pred_prob = clf.predict_proba(test_feature_matrix)
+print(pred_prob)
+
+
+# %matplotlib inline
+# import numpy as np
+
+unique, freq = np.unique(test_labels, return_counts=True)
+freq = list(map(lambda x: x / len(test_labels), freq))
+
+pred_freq = pred_prob.mean(axis=0)
+plt.figure(figsize=(10, 8))
+plt.bar(range(1, 8), pred_freq, width=0.4, align="edge", label='prediction')
+plt.bar(range(1, 8), freq, width=-0.4, align="edge", label='real')
+plt.ylim(0, 0.54)
+plt.legend()
+plt.show()
+print(pred_freq[2])
---
+++ line.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+all_data = pd.read_csv('forest_dataset.csv')
+print(all_data.head())
+
+print(all_data.shape)
+# Столбец '54' - метка класса
+labels = all_data[all_data.columns[-1]].values
+
+# Столбцы'0-54' - признаковые описания
+feature_matrix = all_data[all_data.columns[:-1]].values
+
+# Разделим выборку на обучающую и тестовую
+train_feature_matrix, test_feature_matrix, train_labels, test_labels = \
+    train_test_split(feature_matrix, labels, test_size=0.2, random_state=42)
+
+# Создание модели с указанием гиперпараметра C
+clf = LogisticRegression(C=1)
+clf.max_iter = 10000000
+# Обучение модел
+X_scaled = preprocessing.scale(train_feature_matrix)
+clf.fit(X_scaled, train_labels)
+# Предсказание на тестовой выборке
+y_pred = clf.predict(test_feature_matrix)
+print(y_pred)
+
+print(accuracy_score(test_labels, y_pred))
+
+clf = LogisticRegression(solver='saga')
+# clf.max_iter = 1000
+parad_grid = {
+    'C': np.arange(1, 5),
+    'penalty': ['l1', 'l2'],
+}
+
+search = GridSearchCV(
+    clf, parad_grid, n_jobs=-1, cv=5, refit=True, scoring='accuracy')
+
+search.fit(feature_matrix, labels)
+
+# У меня нашло с кучей варнингов {'C': 2, 'penalty': 'l2'} ghb max_
+# Нашло на юпитере тоже в варниногом {'C': 1, 'penalty': 'l1'}
+
+print(search.best_params_)
+
+
